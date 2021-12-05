0c7d1d752c3b4ea5b4a8a720e6d0f8b570f87d78
kenji <kenjix12x22@gmail.com>
2021-12-06 00:22:11 +0900

    7

---
+++ 7/answer.py
@@ -0,0 +1,5 @@
+N = int(input())
+
+for _ in range(N):
+    s_a = input().split()
+    print(s_a[0], int(s_a[1]) + 1)
---
+++ 7/my.py
@@ -0,0 +1,4 @@
+n = int(input())
+for i in range(n):
+    ans = input().split()
+    print(ans[0] + " " + str(int(ans[1]) + 1))
